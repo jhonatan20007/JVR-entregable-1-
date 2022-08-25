db03195980d361ce26ddd2b101d051fd3cf111d7
jhonatan20007 <86896817+jhonatan20007@users.noreply.github.com>
2022-08-25 11:42:27 -0500

    ajuste
    .

---
+++ js/Utilidades.js
@@ -0,0 +1,63 @@
+document.getElementById("btn_open").addEventListener("click", open_close_menu);
+
+//Declaramos variables
+var side_menu = document.getElementById("menu_side");
+var btn_open = document.getElementById("btn_open");
+var body = document.getElementById("body");
+
+//Evento para mostrar y ocultar menú
+    function open_close_menu(){
+        body.classList.toggle("body_move");
+        side_menu.classList.toggle("menu__side_move");
+    }
+
+//Si el ancho de la página es menor a 760px, ocultará el menú al recargar la página
+
+if (window.innerWidth < 760){
+
+    body.classList.add("body_move");
+    side_menu.classList.add("menu__side_move");
+}
+
+//Haciendo el menú responsive(adaptable)
+
+window.addEventListener("resize", function(){
+
+    if (window.innerWidth > 760){
+
+        body.classList.remove("body_move");
+        side_menu.classList.remove("menu__side_move");
+    }
+
+    if (window.innerWidth < 760){
+
+        body.classList.add("body_move");
+        side_menu.classList.add("menu__side_move");
+    }
+
+});
+
+function ClassOpcion(evt, opcion) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("selected");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace("selected", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(opcion).style.display = "block";
+    evt.currentTarget.className += " selected";
+  }
+
+
+ 
+
